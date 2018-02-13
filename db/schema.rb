6228f5d693db39b7ea5a6ecba244b0f959f0791d
Mathieu Federak <mfederak@gmail.com>
2018-02-13 09:19:35 +0100

    added column address to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213081017) do
+ActiveRecord::Schema.define(version: 20180213081640) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
